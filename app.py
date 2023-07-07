43e610bf1ec6ed08ea0afc7b5d69d14275b76202
ItamiWorld <iris.minami0307@gmail.com>
2023-07-07 17:00:17 +0900

    update: added routing examples

--- app.py
+++ app.py
@@ -2,6 +2,11 @@
 from flask import Flask
 from markupsafe import escape
 from flask import url_for
+from flask import request
+
+###################################################################################################
+#########################################    Example   ############################################
+###################################################################################################
 
 # Create a instance of Flask class. The first argument is the name of the application's module or package.
 # @__name__ argument is convenient shortcut. This is needed to look for resources as templates or static files.
@@ -20,10 +25,11 @@ def string():
 def html():
     return '<p>Hello, world</p>'
 
-
+###################################################################################################
 #########################################    Routing   ############################################
+###################################################################################################
 
-##### Variable Rules #####
+#########################################   Variable Rules   ######################################
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -41,7 +47,7 @@ def show_subpath(subpath):
     return f"Subpath {escape(subpath)}"
 
 
-##### Unique URLs /Redirection Behavior #####
+#########################################   Unique URLs /Redirection Behavior   ###################
 
 # If user access /projects, it will be redirect to /projects/
 @app.route('/projects/')
@@ -54,9 +60,10 @@ def about():
     return "The about page"
 
 
-##### URL Building #####
-@app.route('/')
-def index():
+#########################################   URL Building   ########################################
+
+@app.route('/index')
+def index2():
     return 'index'
 
 @app.route('/login')
@@ -73,3 +80,20 @@ with app.test_request_context():
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
 
+
+#########################################   HTTP Methods   ########################################
+def do_the_login():
+    return 'Logged in'
+
+def show_the_login_form():
+    return 'Try again'
+
+@app.route('/http_login', methods=['GET', 'POST'])
+def http_login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+    
+#########################################   Static Files   #########################################
+url_for('static', filename='style.css')
